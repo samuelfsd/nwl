f624ed396a5b0c8165ec915cab362444f045b6bb
Samuel Alves Medeiros <53972764+samuelfsd@users.noreply.github.com>
2021-01-18 18:50:47 -0300

    Add files via upload

---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: 1,
+        name:"Lar das meninas",
+        lat:"-6.5994013",
+        lng:"-37.6222344",
+        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social ",
+        images:[
+            "https://images.unsplash.com/photo-1554312879-371d7377dea0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1600712243189-aaa2152723b4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours:"Horário de visitas Das 18h até 8h ",
+        open_on_weekends:"1"
+    },
+    {
+        id: 2,
+        name:"Lar dos meninos",
+        lat:"-6.5994013",
+        lng:"-37.6232344",
+        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social ",
+        images:[
+            "https://images.unsplash.com/photo-1554312879-371d7377dea0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1600712243189-aaa2152723b4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+            ],
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours:"Horário de visitas Das 18h até 8h ",
+        open_on_weekends:"0"
+    
+    }
+]
