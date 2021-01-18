65e980221ed7705f2b85b4763d2618fb0a803f16
Samuel Alves Medeiros <53972764+samuelfsd@users.noreply.github.com>
2021-01-18 20:40:14 -0300

    Add files via upload

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,41 @@
+//create map
+const map = L.map('mapid').setView([-6.5994013,-37.6222344], 17);
+
+//create and add titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+}).addTo(map);
+
+//create  icon 
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize:[58,68],
+    iconAnchor:[29,68],
+    popupAnchor:[170,2]
+})
+
+
+function addMarker({ id, name, lat, lng } = orphanage) {
+    const popup = L.popup({
+      closeButton: false,
+      className: "map-popup",
+      minWidth: 240,
+      minHeight: 240,
+    }).setContent(
+      `${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`
+    );
+  
+    L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+  }
+  
+  const orphanagesSpan = document.querySelectorAll(".orphanages span");
+  
+  orphanagesSpan.forEach((span) => {
+    const orphanage = {
+      id: span.dataset.id,
+      name: span.dataset.name,
+      lat: span.dataset.lat,
+      lng: span.dataset.lng,
+    };
+  
+    addMarker(orphanage);
+  });
